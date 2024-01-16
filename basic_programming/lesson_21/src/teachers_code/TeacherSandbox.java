bc7fec8d87dbd42384fd1fa42dd5c535625bc677
MaksymBilousenko <maksimbelousenko11@gmail.com>
2024-01-16 20:56:16 +0100

    new commit

--- basic_programming/lesson_21/src/teachers_code/TeacherSandbox.java
+++ basic_programming/lesson_21/src/teachers_code/TeacherSandbox.java
@@ -1,13 +1,4 @@
 package teachers_code;
 
 public class TeacherSandbox {
-    public static void main(String[] args) {
-        System.out.println("Hello, fork!");
-    }
-
-    static void doSomething() {
-        System.out.println("Hello there!");
-    }
-
-    /// комментарий
 }
--- basic_programming/lesson_26/src/student_code/StudentCode.java
+++ basic_programming/lesson_26/src/student_code/StudentCode.java
@@ -2,6 +2,9 @@ package student_code;
 
 public class StudentCode {
     public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
 
 
     }
