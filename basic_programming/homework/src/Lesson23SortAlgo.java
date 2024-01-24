66a3fcf6e6b57a267566405d9a650e596f154717
MaksymBilousenko <maksimbelousenko11@gmail.com>
2024-01-24 18:35:01 +0100

    new commit

---
+++ basic_programming/homework/src/Lesson23SortAlgo.java
@@ -0,0 +1,3 @@
+public class Lesson23SortAlgo {
+
+}
--- basic_programming/lesson_27/src/student_code/StudentCode.java
+++ basic_programming/lesson_27/src/student_code/StudentCode.java
@@ -1,8 +1,100 @@
 package student_code;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentCode {
     public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+
+        ArrayList<String> stringArrayList = new ArrayList<>(6); // {null, null, null, null,null, null} size = 0
+
+        stringArrayList.add(null); //  size = 1 {null, null, null, null, null, null}
+        stringArrayList.add("one"); // size = 2 {null, one, null, null, null, null}
+        stringArrayList.add("two");
+        stringArrayList.add("three");
+
+        // {one, two, three}
+        //   0     1     2
+        stringArrayList.get(1);
+
+        printArray(stringArrayList);
+
+        stringArrayList.add(null);
+
+        // метод List.of позволяет создать неизменяемую коллекцию из элементов,
+        // то есть из нее ничего нельзя удалить или добавить
+        List<String> immutableArrayList = List.of("four", "five", "six");
+
+        try {
+            immutableArrayList.add("seven");
+
+        } catch (UnsupportedOperationException exception) {
+            if (exception.getMessage() != null) {
+                System.out.println(exception.getMessage());
+            } else {
+                System.out.println("не получилось добавить новый элемент");
+            }
+        }
+
+        stringArrayList.addAll(immutableArrayList);
+
+        printArray(stringArrayList);
+
+        stringArrayList.add(2, "seven");
+
+        printArray(stringArrayList);
+
+        stringArrayList.set(2, "eight");
+
+        printArray(stringArrayList);
+
+        stringArrayList.remove(3);
+
+        printArray(stringArrayList);
+
+        stringArrayList.add(2, "four");
+        stringArrayList.add(2, "four");
+        stringArrayList.add(2, "four");
+
+        printArray(stringArrayList);
+
+        stringArrayList.remove("four");
+
+        printArray(stringArrayList);
 
 
+        // удаляет все элементы, содержащиеся в immutableArrayList из stringArrayList
+        try {
+            stringArrayList.removeAll(immutableArrayList);
+        } catch (NullPointerException e) {
+            System.out.println("данная операция работает только если в коллекйии нет значений null");
+        }
+
+        printArray(stringArrayList);
+
+        System.out.println("stringArrayList.contains(\"four\") = " + stringArrayList.contains("four"));
+
+        for (int i = 0; i < stringArrayList.size(); i++) {
+            if (!stringArrayList.contains("four")) {
+                break;
+            } else {
+                stringArrayList.remove("four");
+            }
+        }
+        printArray(stringArrayList);
+
+        while (stringArrayList.contains("four")) {
+            stringArrayList.remove("four");
+        }
     }
+
+    static void printArray(ArrayList list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+
 }
