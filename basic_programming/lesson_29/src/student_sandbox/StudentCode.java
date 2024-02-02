075122de3de4f4deb7d163851ac0bf6863bb3ba4
MaksymBilousenko <maksimbelousenko11@gmail.com>
2024-02-02 18:30:03 +0100

    new commit

--- basic_programming/lesson_29/src/student_sandbox/StudentCode.java
+++ basic_programming/lesson_29/src/student_sandbox/StudentCode.java
@@ -2,7 +2,21 @@ package student_sandbox;
 
 public class StudentCode {
     public static void main(String[] args) {
+        Integer a = 20;
+        Integer b = 20;
 
+        System.out.println("a.hashCode() = " + a.hashCode());
+        System.out.println("b.hashCode() = " + b.hashCode());
+        System.out.println("a.equals(b) = " + a.equals(b));
+
+        String str1 = "hello";
+        String str2 = new String("hello");
+        System.out.println("str1.hashCode() = " + str1.hashCode());
+        System.out.println("str2.hashCode() = " + str2.hashCode());
+        System.out.println("str1.equals(str2) = " + str1.equals(str2));
+
+        int[] nums1 = {1, 2, 3};
+        int[] nums2 = {1, 2, 3};
 
     }
 }
--- basic_programming/lesson_30/src/student_code/StudentCode.java
+++ basic_programming/lesson_30/src/student_code/StudentCode.java
@@ -1,4 +1,27 @@
 package student_code;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class StudentCode {
+    public static void main(String[] args) {
+        Map<String, String> countries = new HashMap<>();
+
+        //добовлять можно исключительно парой -
+        // нельзя добавить только ключ или только значение
+        countries.put("Germany", "Berlin");
+        countries.put("Austrie", "Vien");
+        countries.put("France", "Paris");
+        countries.put("Great Britain", "London");
+
+        // чтобы получить все ключи, мы можем воспользоваться методом keySet(),
+        // который вернет set, содержащий ключи из мапы
+
+        Set<String> keys = countries.keySet();
+
+        for (String key : keys) {
+            System.out.println(countries.get(key));
+        }
+    }
 }
