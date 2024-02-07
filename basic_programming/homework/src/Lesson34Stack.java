ab0678e2bf1705a487d6fd61c5e4f3377ccf28a2
MaksymBilousenko <maksimbelousenko11@gmail.com>
2024-02-07 18:46:21 +0100

    new commit

---
+++ basic_programming/homework/src/Lesson34Stack.java
@@ -0,0 +1,86 @@
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Lesson34Stack {
+    mjn // метод проверяет скобочную последовательность, если последовательность верна возвращаем true, если нет false
+    public static boolean isValid(String string) {
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < string.length(); i++) {
+            // используется для преобразования символа, находящегося в строке string по индексу i, в строку (можно сказать,
+            // мы делим строку по символьно)
+            String bracket = Character.toString(string.charAt(i));
+            if (bracket.equals("(") || bracket.equals("[") || bracket.equals("{") || bracket.equals("<")) {
+                stack.push(bracket);
+            } else if (bracket.equals(")") || bracket.equals("]") || bracket.equals("}") || bracket.equals(">")) {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                switch (bracket) {
+                    case ")":
+                        if (stack.pop().equals("(")) {
+                        } else {
+                            return false;
+                        }
+                        break;
+                    case "]":
+                        if (stack.pop().equals("[")) {
+                        } else {
+                            return false;
+                        }
+                        break;
+                    case "}":
+                        if (stack.pop().equals("{")) {
+                        } else {
+                            return false;
+                        }
+                        break;
+                    case ">":
+                        if (stack.pop().equals("<")) {
+                        } else {
+                            return false;
+                        }
+                        break;
+                }
+
+
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+
+class BrecketValidTest {
+    @Test
+    public void testValid() {
+        assertTrue(Lesson34Stack.isValid("()"));
+        assertTrue(Lesson34Stack.isValid("[]"));
+        assertTrue(Lesson34Stack.isValid("{}"));
+        assertTrue(Lesson34Stack.isValid("<>"));
+        assertTrue(Lesson34Stack.isValid("{(<{}[]>)}"));
+    }
+    @Test
+    public void testInvalid() {
+        assertFalse(Lesson34Stack.isValid("("));
+        assertFalse(Lesson34Stack.isValid(")"));
+        assertFalse(Lesson34Stack.isValid("["));
+        assertFalse(Lesson34Stack.isValid("]"));
+        assertFalse(Lesson34Stack.isValid("{"));
+        assertFalse(Lesson34Stack.isValid("}"));
+        assertFalse(Lesson34Stack.isValid("<"));
+        assertFalse(Lesson34Stack.isValid(">"));
+        assertFalse(Lesson34Stack.isValid("(([))"));
+        assertFalse(Lesson34Stack.isValid(")("));
+        assertFalse(Lesson34Stack.isValid("{}[]())"));
+    }
+    @Test
+    public void testEmptyString() {
+        assertTrue(Lesson34Stack.isValid(""));
+
+    }
+}
+
--- basic_programming/lesson_34/src/practice/StackQueueExercises.java
+++ basic_programming/lesson_34/src/practice/StackQueueExercises.java
@@ -1,9 +1,21 @@
 package practice;
 
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
 public class StackQueueExercises {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+        System.out.println("queue = " + queue);
+        reverseQueue(queue);
+    }
 
     /**
      * Задача 1: Реверс элементов в очереди.<p>
@@ -11,7 +23,15 @@ public class StackQueueExercises {
      * @param queue Очередь, элементы которой нужно реверсировать.
      */
     public static void reverseQueue(Queue<Integer> queue) {
-
+        Stack<Integer> stack = new Stack<>();
+        // 1, 2, 3
+        while (!queue.isEmpty()) {
+            stack.push(queue.poll());
+        }
+        while (!stack.isEmpty()) {
+            queue.add(stack.pop());
+        }
+        System.out.println("queue = " + queue);
     }
 
     /**
