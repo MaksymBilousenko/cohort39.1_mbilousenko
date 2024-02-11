a6f909ad99a0c370a9911d90c571782a7483951b
MaksymBilousenko <maksimbelousenko11@gmail.com>
2024-02-11 18:58:26 +0100

    new commit

--- basic_programming/homework/src/Lesson34Stack.java
+++ basic_programming/homework/src/Lesson34Stack.java
@@ -1,13 +1,27 @@
 
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
 import java.util.Stack;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Lesson34Stack {
-    mjn // метод проверяет скобочную последовательность, если последовательность верна возвращаем true, если нет false
+    public static void main(String[] args) {
+        try (FileReader fileReader = new FileReader("bracket.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // метод проверяет скобочную последовательность, если последовательность верна возвращаем true, если нет false
     public static boolean isValid(String string) {
         Stack<String> stack = new Stack<>();
         for (int i = 0; i < string.length(); i++) {
@@ -63,6 +77,7 @@ class BrecketValidTest {
         assertTrue(Lesson34Stack.isValid("<>"));
         assertTrue(Lesson34Stack.isValid("{(<{}[]>)}"));
     }
+
     @Test
     public void testInvalid() {
         assertFalse(Lesson34Stack.isValid("("));
@@ -77,6 +92,7 @@ class BrecketValidTest {
         assertFalse(Lesson34Stack.isValid(")("));
         assertFalse(Lesson34Stack.isValid("{}[]())"));
     }
+
     @Test
     public void testEmptyString() {
         assertTrue(Lesson34Stack.isValid(""));
--- basic_programming/lesson_35/src/student_code/StudentCode.java
+++ basic_programming/lesson_35/src/student_code/StudentCode.java
@@ -1,7 +1,13 @@
 package student_code;
 
+import java.time.LocalDateTime;
+
 public class StudentCode {
     public static void main(String[] args) {
+        LocalDateTime ldt = LocalDateTime.now();
+        LocalDateTime firstDayOfYear = ldt.withDayOfYear(1);
+        LocalDateTime ldt2 = firstDayOfYear.plusHours(1000);
+        System.out.println("ldt2.getDayOfWeek() = " + ldt2.getDayOfWeek());
 
     }
 }
